a00a738771e5e0fcc2e5f09a582ceea4bbd83db1
Wesley Almeida <wesleymna98@gmail.com>
2025-01-25 10:55:06 -0300

    feat: singleton pattern removed from CommandFactory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import shell.Shell;
 public class Main {
 
     public static void main(String[] args) {
-        var factory = CommandFactory.getInstance();
+        var factory = new CommandFactory();
         new Shell(factory).startRepl();
     }
 }
--- src/main/java/commands/CommandFactory.java
+++ src/main/java/commands/CommandFactory.java
@@ -1,29 +1,21 @@
 package commands;
 
+import commands.builtin.EchoCommand;
+import commands.builtin.ExitCommand;
+import commands.builtin.TypeCommand;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 public class CommandFactory {
 
-    private static CommandFactory instance;
     private final Map<String, Command> builtins = new HashMap<>();
 
-    private CommandFactory() {
+    public CommandFactory() {
         builtins.put("exit", new ExitCommand());
         builtins.put("echo", new EchoCommand());
         builtins.put("type", new TypeCommand(this));
-    }
-
-    public static CommandFactory getInstance() {
-        if (instance == null)
-            instance = new CommandFactory();
-
-        return instance;
-    }
-
-    public void registerBuiltin(String name, Command command) {
-        builtins.put(name, command);
     }
 
     public Optional<Command> getCommand(String name) {
@@ -31,9 +23,5 @@ public class CommandFactory {
         return command != null
                 ? Optional.of(command)
                 : Optional.empty();
-    }
-
-    public boolean contains(String command) {
-        return builtins.containsKey(command);
     }
 }
