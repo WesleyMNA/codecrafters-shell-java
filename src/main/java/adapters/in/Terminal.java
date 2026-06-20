1e6c5ab79114c67b2fea35e9d87feb10896af405
Wesley Almeida <wesleymna98@gmail.com>
2026-06-20 08:54:39 -0300

    read prompt

--- src/main/java/adapters/in/Terminal.java
+++ src/main/java/adapters/in/Terminal.java
@@ -1,8 +1,13 @@
 package adapters.in;
 
+import java.util.Scanner;
+
 public class Terminal {
 
     public void run() {
         System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        System.out.printf("%s: command not found", command);
     }
 }
